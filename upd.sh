45474e641b2a37a8124d6b24888c1cf5fc17d3fe
Edgar Ardon <eardon294@gmail.com>
2023-03-26 11:02:50 -0600

    Updating Upd script - simplifications

--- upd.sh
+++ upd.sh
@@ -1,30 +1,21 @@
 #!/bin/bash
-#this program will execute update & upgrade repositories using the apt command
-#Addtionaly, it will keep a dated installation log for later review (if needed)
-#So that the screen is clean during system upgrading
-#Recommende Usage#
-#It is recommended to create an Alias for this command and store it in your
-#.bashrc configurations, so that this runs automatically when typing upd, for example.
 
-#release_file=/etc/os-release
+release_file=/etc/os-release
 
-if [ -d /etc/apt/ ]
+if grep -q "ARCH" $release_file
+
+#if [ -d /etc/pacman.d ]
+then
+	sudo pacman -Syu
+fi
+
+
+if grep -q "DEBIAN" $release_file || grep -q "UBUNTU" $release_file
 then	
 	clear
-	notify-send "Advance Package Tool Found"
+	notify-send "Updating Repositories"
 	sleep 2
-	notify-send "updating repositories and upgrading programs"
-	sleep 2
-	notify-send "you may need to enter your credentials"
-	sleep 1
-	echo "----------------------------" >> ~/myrepo/package_install_results.log
-	date >> ~/myrepo/package_install_results.log
-	echo "----------------------------" >> ~/myrepo/package_install_results.log
-	sudo apt update -y  >> ~/myrepo/package_install_results.log
-	sudo apt upgrade -y >> ~/myrepo/package_install_results.log
-	echo "----------------------------" >> ~/myrepo/package_install_results.log
-	notify-send "Your system is up-to-date"
-	sleep 2
+	sudo apt update
+	sudo apt upgrade -y 1> ~/update.log 2>~/error.log
+	notify-send "Updates Completed"
 fi
-notify-send "Review installation logs ~/myrepo/package_install_results.log"
-ls -la ~/myrepo/package_install_results.log
